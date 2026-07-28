6178efae2adc5deb60542757497c385f879b7feb
James (PD) <69664712+JamesDPC@users.noreply.github.com>
2026-07-28 14:45:23 +1000

    Update expected value for test to align with default property value

--- tests/ExitButtonTest.php
+++ tests/ExitButtonTest.php
@@ -67,7 +67,7 @@ class ExitButtonTest extends SapphireTest
         $button->setLabel($label);
 
         $value = $button->forTemplate();
-        $expected = '<div class="exit-button-wrapper"><a id="' . $id . '" class="page-exit" data-url="' . $url . '" href="' . $url . '" rel="nofollow noopener"><span>' . $label . '</span></a></div>';
+        $expected = '<div class="exit-button-wrapper"><a id="' . $id . '" class="page-exit" data-url="' . $url . '" href="' . $url . '" rel="nofollow noopener" data-use-esc="1"><span>' . $label . '</span></a></div>';
         $this->assertEquals($expected, trim((string) $value));
     }
 }
